d1c542475b49b2bd2939c7ea3597d10e1945d0dc
léonie gondo <leonigondo@gmail.com>
2024-12-03 01:18:39 +0000

    dernier ajout

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+//pour les heures
+app.use((req, res, next) => {
+    const now = new Date();
+    const day = now.getDay(); 
+    const hour = now.getHours();
+
+    if (day >= 1 && day <= 5 && hour >= 9 && hour <= 17) {
+        next(); 
+    } else {
+        res.send("<h1>Le site est accessible uniquement du lundi au vendredi, de 9h à 17h.</h1>");
+    }
+})
+
+app.get("/", (req,res) => {
+    res.render('home')
+})
+
+app.get('/services', (req, res) => {
+    res.render('services')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+
+
+
+app.listen(3000, () => console.log("Serveur lancer au port 3000"))
